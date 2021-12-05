7a1961b245c5d56ace7a5e1bda5b1eee290c8c22
hieudz2000 <duchieu20002501@gmail.com>
2021-12-06 01:52:03 +0700

    add name age to user

--- db/schema.rb
+++ db/schema.rb
@@ -28,5 +28,8 @@ ActiveRecord::Schema.define(version: 2021_12_05_175411) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.string "name"
+    t.integer "age"
+    t.index ["name"], name: "index_users_on_name", unique: true
   end
 end
